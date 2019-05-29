cb4891b3c5ac830a028a304c0db45217eb21c25d
NambiarSidharth <32272949+NambiarSidharth@users.noreply.github.com>
2019-05-29 09:50:34 +0530

    added state functions

--- src/Store/actions/songAction.js
+++ src/Store/actions/songAction.js
@@ -1,15 +1,52 @@
 import node from "../../utils/IPFS"
+import {GET_SONGS,GET_SONG} from "./types"
 //universal list for all songs existing
-export const getSongs = (userSession)=>dispatch=>{
+export const getUserSongs = (userSession)=>dispatch=>{
+    const options = { decrypt: false };
+        userSession.getFile(`my_songs.json`, options)
+        .then((content) => {
+            console.log(content)
+          if(content) {
+            posts = JSON.parse(content)
 
+          } else {
+            posts =[] 
+          }
+          console.log(posts)
+        dispatch({
+            type:GET_SONGS,
+            payload:posts
+        })
+        }).catch(err=>{
+            console.log(err)
+        })
 }
 export const getSongsByGenre =()=>dispatch=>{
 
 }
-export const getSong = ()=>dispatch=>{
-
+export const getSong = (identifier,list)=>dispatch=>{
+list.forEach(obj=>{
+    if(obj.id===identifier){
+        dispatch({
+            type:GET_SONG,
+            payload:obj
+        })
+    }
+})
+}
+export const addUserSong = (data,userData)=>dispatch=>{
+//add blockchain broadcast here
+const options = { encrypt: false }
+userData.putFile('my_songs.json',JSON.stringify(data),options)
+.then(obj=>{
+    dispatch({
+        type:GET_SONGS,
+        payload:data
+    })
+})
 }
 //blockchain thing
 export const allsongs = ()=>dispatch=>{
 
 }
+
