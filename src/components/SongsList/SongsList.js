1a9b949f9cacf77f2f5518eb997fbad4c0c07625
NambiarSidharth <32272949+NambiarSidharth@users.noreply.github.com>
2019-05-29 10:25:29 +0530

    data layout of songs list done

--- src/components/SongsList/SongsList.js
+++ src/components/SongsList/SongsList.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {getUserSongs} from "../../Store/actions/songAction"
+import PropTypes from "prop-types"
 export class SongsList extends Component {
+  componentDidMount(){
+    const {userSession} = this.props.auth
+    this.props.getUserSongs(userSession)
+  }
   render() {
     return (
       <div>
@@ -8,5 +15,8 @@ export class SongsList extends Component {
     )
   }
 }
-
-export default SongsList
+const mapStateToProps = state=>({
+  song:state.song,
+  auth:state.auth
+})
+export default connect(mapStateToProps,{getUserSongs})(SongsList)
